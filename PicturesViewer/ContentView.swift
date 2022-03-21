babcab7811666ddedaf1aa2338754b1a61b1620b
Bryan Zec <zecbryan@gmail.com>
2022-03-21 21:01:55 +0100

    Initial Commit

---
+++ PicturesViewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PicturesViewer
+//
+//  Created by Bryan Zec on 21/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PicturesViewer/PicturesViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicturesViewerApp.swift
+//  PicturesViewer
+//
+//  Created by Bryan Zec on 21/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct PicturesViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
