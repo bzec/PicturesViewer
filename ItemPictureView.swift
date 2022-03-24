261eefb9e34919cc3d2a99416e5bcaba78ad3ad2
Bryan Zec <zecbryan@gmail.com>
2022-03-24 21:37:20 +0100

    add arrow to change image to display

---
+++ ItemPictureView.swift
@@ -0,0 +1,20 @@
+//
+//  ItemPictureView.swift
+//  PicturesViewer
+//
+//  Created by Bryan Zec on 22/03/2022.
+//
+
+import SwiftUI
+
+struct ItemPictureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemPictureView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemPictureView()
+    }
+}
--- PicturesViewer/ListPicturesView.swift
+++ PicturesViewer/ListPicturesView.swift
@@ -8,20 +8,49 @@
 import SwiftUI
 
 struct ListPicturesView: View {
-    @Binding var images :  Array<UIImage>?
+    @Binding var images : Array<UIImage>!
+    @State var imageIndex : Int = 0
+
     var body: some View {
-        if images != nil {
-            ForEach(images!, id: \.self) { image in
-                Image(uiImage: image)
-                  .resizable()
-                  .aspectRatio(contentMode: .fit)
-                  .frame(maxWidth: 200, maxHeight: 200)
+        if images.count > 0 {
+            HStack {
+                Button(action: {
+                    print("record clicked")
+                    if(imageIndex > 0) {
+                        print("ron remove")
+
+                        imageIndex-=1
+                    }
+                }){
+                    Image(systemName: "chevron.compact.left")
+                    Text("")
+                        .padding()
+                }
+
+                Image(uiImage: images[imageIndex])
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: 200, maxHeight: 200)
+               
+                Button(action: {
+                    print("record clicked")
+                    if(imageIndex < images.count - 1) {
+                        print("ron addd")
+
+                        imageIndex+=1
+                    }
+                }){
+                    Image(systemName: "chevron.compact.right")
+                    Text("")
+                        .padding()
+                }
             }
         }
     }
 }
 
-struct ListPicturesView_Previews: PreviewProvider {    @State static var prev: Array<UIImage>? = []
+struct ListPicturesView_Previews: PreviewProvider {
+    @State static var prev: Array<UIImage>? = []
 
     static var previews: some View {
         ListPicturesView(images : $prev)
