5d6b15ebf734ab937031edaa5da194f609031c1e
Bryan Zec <zecbryan@gmail.com>
2022-03-28 22:02:59 +0200

    improve gesture image zoom

--- ModalImageView.swift
+++ ModalImageView.swift
@@ -9,22 +9,57 @@ import SwiftUI
 
 struct ModalImageView: View {
     @Environment(\.presentationMode) var presentationMode
+    
     var image: UIImage
-    @GestureState var scale: CGFloat = 1.0
+    
+    @State var scale = 1.0
+    @State private var lastScale = 1.0
 
+    private let minScale = 1.0
+    private let maxScale = 5.0
+    
     var body: some View {
         Image(uiImage: image)
-            .resizable().aspectRatio(contentMode: .fit)
-            .scaleEffect(scale)
-            .gesture(MagnificationGesture()
-                .updating($scale, body: { (value, scale, trans) in
-                    scale = value.magnitude
-                })
-            )
+            .resizable()
+              .aspectRatio(contentMode: .fit)
+              .scaleEffect(scale)
+              .gesture(MagnificationGesture()
+                .onChanged { state in
+                    adjustScale(from: state)
+                }
+                .onEnded { state in
+                    withAnimation {
+                        validateScaleLimits()
+                    }
+                    lastScale = 1.0
+                    
+                }
+         )
         Text("Close Modal")
             .onTapGesture {
+                // change scale value to fix animation image bug when we dismiss value
+                scale = minScale
                 presentationMode.wrappedValue.dismiss()
             }
+    }
+
+    func adjustScale(from state: MagnificationGesture.Value) {
+        let delta = state / lastScale
+        scale *= delta
+        lastScale = state
+    }
+    
+    func getMinimumScaleAllowed() -> CGFloat {
+        return max(scale, minScale)
+    }
+    
+    func getMaximumScaleAllowed() -> CGFloat {
+        return min(scale, maxScale)
+    }
+    
+    func validateScaleLimits() {
+        scale = getMinimumScaleAllowed()
+        scale = getMaximumScaleAllowed()
     }
 }
 
